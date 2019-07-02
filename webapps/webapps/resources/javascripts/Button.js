3ed3217acf423b0f7b71dd104f6ca3038779cfa7
linkqu <hongquanli@qq.com>
2019-07-02 11:51:42 +0800

    //

--- webapps/webapps/resources/javascripts/Button.js
+++ webapps/webapps/resources/javascripts/Button.js
@@ -1,81 +1,93 @@
 // import {a} from './xxx.js'
 // import buttonCss from "../themes/Button.css.js";
 
+import {Component} from "./Component";
+import type {Structure} from "./Component";
+
 const template = document.createElement('template');
 template.innerHTML = `
     <style></style>
     <!--<a href="#">Link</a>-->
 `;
 
-class Button extends HTMLElement {
+const buttonStructure = {
+    'type': 'button'
+};
 
-    constructor() {
-        // Always call super first in constructor
-        super();
-
-        // Create a shadow root. open/closed
-        const shadow = this.attachShadow({mode: 'open'});
-
-        // Create button
-        const button = document.createElement('button');
-        button.setAttribute('class', 'ui button');
-        button.classList.add(this.getAttribute('data-style'));
-
-        const icon = document.createElement('i');
-        icon.setAttribute('class', 'spider icon');
-        // icon.setAttribute('tabindex', 0);
-        // icon.removeAttribute();
-
-        // Take attribute content and put it inside the info span
-        button.textContent = this.getAttribute('data-text');
-
-        // Create some CSS to apply to the shadow dom
-        const style = document.createElement('style');
-        console.log(style.isConnected);
-
-        button.addEventListener('click', (e) => {
-            alert("Hello!")
-        });
-
-        style.textContent = `@import "./resources/stylesheets/Button.css"`;
-
-        // Templates
-        // template.content;
-        // const template = document.getElementById('element-details-template').content;
-        // const shadowRoot = this.attachShadow({mode: 'open'}).appendChild(template.cloneNode(true));
-        shadow.appendChild(template.content);
-
-        // Attach the created elements to the shadow dom
-        shadow.appendChild(style);
-        shadow.appendChild(button);
-        button.appendChild(icon);
-
-        /*
-        const button = document.querySelector('button');
-        const root = button.createShadowRoot();
-        root.innerHTML = '<style>' +
-            ':host { text-transform: uppercase; }' +
-            '</style>' +
-            '<content></content>';
-        */
-    }
-
-    connectedCallback() {
-        console.log('Custom square element added to page.');
-    }
-
-    disconnectedCallback() {
-        console.log('Custom square element removed from page.');
-    }
-
-    adoptedCallback() {
-        console.log('Custom square element moved to new page.');
-    }
-
-    attributeChangedCallback(name, oldValue, newValue) {
-        console.log('Custom square element attributes changed.');
-    }
+interface Properties {
+    ripple: boolean,
+    raised: boolean,
+    dense: boolean,
+    icon: string
 }
+
+interface Data {
+
+}
+
+class Button extends Component<Properties, Data> {
+
+    static defaultProperties = {
+        ripple: true
+    };
+
+    getStructure(): Array<Structure> {
+        let structure: Array<Structure> = [];
+        Object.assign(buttonStructure, structure);
+        return structure;
+    }
+
+    // constructor() {
+    //     // Always call super first in constructor
+    //     super();
+    //
+    //     // Create a shadow root. open/closed
+    //     const shadow = this.attachShadow({mode: 'open'});
+    //
+    //     // Create button
+    //     const button = document.createElement('button');
+    //     button.setAttribute('class', 'ui button');
+    //     button.classList.add(this.getAttribute('data-style'));
+    //
+    //     const icon = document.createElement('i');
+    //     icon.setAttribute('class', 'spider icon');
+    //     // icon.setAttribute('tabindex', 0);
+    //     // icon.removeAttribute();
+    //
+    //     // Take attribute content and put it inside the info span
+    //     button.textContent = this.getAttribute('data-text');
+    //
+    //     // Create some CSS to apply to the shadow dom
+    //     const style = document.createElement('style');
+    //     console.log(style.isConnected);
+    //
+    //     button.addEventListener('click', (e) => {
+    //         alert("Hello!")
+    //     });
+    //
+    //     style.textContent = `@import "./resources/stylesheets/Button.css"`;
+    //
+    //     // Templates
+    //     // template.content;
+    //     // const template = document.getElementById('element-details-template').content;
+    //     // const shadowRoot = this.attachShadow({mode: 'open'}).appendChild(template.cloneNode(true));
+    //     shadow.appendChild(template.content);
+    //
+    //     // Attach the created elements to the shadow dom
+    //     shadow.appendChild(style);
+    //     shadow.appendChild(button);
+    //     button.appendChild(icon);
+    //
+    //     // const button = document.querySelector('button');
+    //     // const root = button.createShadowRoot();
+    //     // root.innerHTML = '<style>' +
+    //     //     ':host { text-transform: uppercase; }' +
+    //     //     '</style>' +
+    //     //     '<content></content>';
+    // }
+}
+
+export default {Button};
 
 // Define the new element
 customElements.define('ui-button', Button);
--- webapps/webapps/resources/javascripts/Component.js
+++ webapps/webapps/resources/javascripts/Component.js
@@ -1,5 +1,145 @@
-class Component {
+import _ from 'lodash'
 
+export interface Event {
+    type: string,
+    listener: EventListenerOrEventListenerObject,
+    options?: boolean | AddEventListenerOptions
+}
+
+export interface Structure {
+    type: string,
+    text?: string,
+    attributes?: Map,
+    events?: Array<Event>,
+    children?: Array<Structure>,
+}
+
+class Component extends HTMLElement {
+
+    // get structure(): Array<Structure>;
+
+    // set structure(value: Array<Structure>);
+
+    constructor(properties, data?) {
+        // Always call super first in constructor
+        super();
+
+        // const firstName = message?.body?.user?.firstName || 'default';
+
+        this.properties = Object.assign(
+            structureProperties(properties),
+            this.constructor.defaultProperties
+        );
+
+        this.data = data || {};
+    }
+
+    connectedCallback() {
+        console.log('Custom square element added to page.');
+
+        // Create a shadow root. open/closed
+        const shadow = this.attachShadow({mode: 'open'});
+        buildStructure(this.getStructure());
+    }
+
+    disconnectedCallback() {
+        console.log('Custom square element removed from page.');
+    }
+
+    adoptedCallback() {
+        console.log('Custom square element moved to new page.');
+    }
+
+    attributeChangedCallback(name, oldValue, newValue) {
+        console.log('Custom square element attributes changed.');
+    }
+
+    getStructure(): Array<Structure> {
+
+    }
+
+    removeAttribute(key) {
+        super.removeAttribute(key);
+    }
+
+    setAttribute(key, value) {
+        if (value && typeof value === 'object') {
+            super.setAttribute(key, JSON.stringify(value));
+        } else {
+            super.setAttribute(key, value);
+        }
+    }
+
+    initialize() {
+
+    }
+
+    update() {
+
+    }
+
+    dispose() {
+
+    }
+
+    before() {
+
+    }
+
+    after() {
+
+    }
+
+}
+
+export function buildStructure(structure: Array<Structure>): HTMLElement {
+
+    if(structure) return {};
+
+    let elementObject: HTMLElement;
+
+    structure.forEach($ => {
+        elementObject = document.createElement($.type);
+
+        if ($.attributes) {
+            $.attributes.forEach((key, value) => {
+                elementObject.setAttribute(
+                    key, $.attributes[value]
+                );
+            });
+        }
+
+        if ($.text) {
+            let textNode = document.createTextNode(text);
+            elementObject.appendChild(textNode);
+        }
+
+        if ($.events) {
+            $.events.forEach((key, value) => {
+                elementObject.addEventListener(
+                    key, $.events[value]
+                );
+            });
+        }
+
+        if ($.children) {
+            elementObject.appendChild(this.buildStructure($.children));
+        }
+    });
+    return elementObject? elementObject : {};
+}
+
+export function isArray(obj) {
+    return Object.prototype.toString.call(obj) === '[object Array]'
+}
+
+export function structureProperties(props) {
+    if (!props || isArray(props)) return {};
+    const result = {};
+    Object.keys(props).forEach(key => {
+        result[key] = props[key].value
+    });
+    return result
 }
 
 export {Component};
